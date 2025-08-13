a915e2ff578d055f2f4bed5dcf53db2e973be3be
Adi <adisetiono1911@gmail.com>
2025-08-13 09:53:26 +0700

    feat(change notif): ubah notif menggunakan sonner

--- resources/js/layouts/AppLayout.vue
+++ resources/js/layouts/AppLayout.vue
@@ -1,34 +1,46 @@
 <script setup lang="ts">
-import GlobalAlert from '@/components/GlobalAlert.vue';
 import ModalManager from '@/components/ModalManager.vue';
+import { Toaster } from '@/components/ui/sonner';
 import AppLayout from '@/layouts/app/AppSidebarLayout.vue';
-import { useAlertStore, type AlertConfig } from '@/stores/alertStore';
+// import { useAlertStore, type AlertConfig } from '@/stores/alertStore';
 import type { BreadcrumbItemType } from '@/types';
 import { usePage } from '@inertiajs/vue3';
-import { computed, watch } from 'vue';
+import { watch } from 'vue';
+import { toast } from 'vue-sonner';
+import 'vue-sonner/style.css'; //
 
 const page = usePage();
-const alertStore = useAlertStore();
+// const alertStore = useAlertStore();
 
 // Buat computed property untuk mendapatkan flash message secara reaktif.
-const flash = computed(() => page.props.flash as { message?: string; variant?: AlertConfig['variant']; title?: string } | null);
+// const flash = computed(() => page.props.flash as { message?: string; variant?: AlertConfig['variant']; title?: string } | null);
 
-// Awasi perubahan pada computed property `flash`.
 watch(
-    flash,
-    (newFlash) => {
-        if (newFlash?.message) {
-            alertStore.showAlert({
-                title: newFlash.title || 'Notifikasi',
-                description: newFlash.message,
-                variant: newFlash.variant || 'default',
-            });
+    () => page.props.flash as { message?: string; variant?: 'default' | 'destructive'; title?: string },
+    (flash) => {
+        if (flash?.message) {
+            if (flash.variant === 'destructive') {
+                toast.error(flash.title || 'Notifikasi', {
+                    description: flash.message,
+                    action: {
+                        label: 'OK',
+                        onClick: () => console.log('OK'),
+                    },
+                });
+            } else {
+                toast.success(flash.title || 'Notifikasi', {
+                    description: flash.message,
+                    action: {
+                        label: 'OK',
+                        onClick: () => console.log('OK'),
+                    },
+                });
+            }
+            // Hapus flash message setelah ditampilkan
             page.props.flash = null;
         }
     },
-    {
-        immediate: true,
-    },
+    { deep: true },
 );
 
 interface Props {
@@ -42,8 +54,8 @@ withDefaults(defineProps<Props>(), {
 
 <template>
     <AppLayout :breadcrumbs="breadcrumbs">
-        <GlobalAlert />
         <slot />
         <ModalManager />
+        <Toaster />
     </AppLayout>
 </template>
